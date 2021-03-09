15d751f8bb07060216917ac189084ca8af3e2830
imhilla <hillaryodhiambo@gmail.com>
2021-03-09 21:27:04 +0300

    add close icon button function

--- src/Preview.js
+++ src/Preview.js
@@ -1,22 +1,28 @@
-import React, {useEffect} from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
-import { selectCameraImage } from './features/cameraSlice';
+import { resetCameraImage, selectCameraImage } from './features/cameraSlice';
 import './Preview.css';
+import CloseIcon from '@material-ui/icons/Close'
 
 
 function Preview() {
   const cameraImage = useSelector(selectCameraImage)
-  const history = useHistory
+  const history = useHistory();
+  const dispatch = useDispatch();
   useEffect(() => {
-    if(!cameraImage) {
-     history.replace('/')
+    if (!cameraImage) {
+      history.replace('/')
     }
- 
-  }, [])
+  }, [cameraImage, history])
+
+  const closePreview = ()=>{
+    dispatch(resetCameraImage());
+  }
 
   return (
     <div className="preview">
+      <CloseIcon onClick={closePreview} className="preview__close" />
       <img src={cameraImage} alt="" />
     </div>
   )
