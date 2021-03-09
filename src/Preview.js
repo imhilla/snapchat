310e3a16fb1c80f0276ded1d976706c8864d09f5
imhilla <hillaryodhiambo@gmail.com>
2021-03-09 21:44:42 +0300

    add more preview icons

--- src/Preview.js
+++ src/Preview.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import { resetCameraImage, selectCameraImage } from './features/cameraSlice';
 import './Preview.css';
-import CloseIcon from '@material-ui/icons/Close'
+import { Close, AttachFile, Create, Crop, MusicNote, Note, Timer, TextFields } from '@material-ui/icons';
 
 
 function Preview() {
@@ -16,13 +16,22 @@ function Preview() {
     }
   }, [cameraImage, history])
 
-  const closePreview = ()=>{
+  const closePreview = () => {
     dispatch(resetCameraImage());
   }
 
   return (
     <div className="preview">
-      <CloseIcon onClick={closePreview} className="preview__close" />
+      <Close onClick={closePreview} className="preview__close" />
+      <div className="preview__toolbarRight">
+        <TextFields />
+        <Create />
+        <Note />
+        <MusicNote />
+        <AttachFile />
+        <Crop />
+        <Timer />
+      </div>
       <img src={cameraImage} alt="" />
     </div>
   )
