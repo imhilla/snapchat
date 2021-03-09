2ae0a34cd50b06f32a050866cc1fefcdcf7c804d
imhilla <hillaryodhiambo@gmail.com>
2021-03-09 21:16:00 +0300

    add pushback if no camera image

--- src/Preview.js
+++ src/Preview.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import { selectCameraImage } from './features/cameraSlice';
 import './Preview.css';
 
+
 function Preview() {
+  const cameraImage = useSelector(selectCameraImage)
+  const history = useHistory
+  useEffect(() => {
+    if(!cameraImage) {
+     history.replace('/')
+    }
+ 
+  }, [])
+
   return (
     <div className="preview">
-      
+      <img src={cameraImage} alt="" />
     </div>
   )
 }
