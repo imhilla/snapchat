23dfc87a14e7447a4b7c852b311a3ac8d5a349cb
imhilla <hillaryodhiambo@gmail.com>
2021-03-10 12:18:33 +0300

    add react time ago

--- src/Chatone.js
+++ src/Chatone.js
@@ -1,15 +1,18 @@
 import { Avatar } from '@material-ui/core';
+import { StopRounded } from '@material-ui/icons';
 import React from 'react';
-import './Chat.css'
+import './Chat.css';
+import ReactTimeAgo from 'react-time-ago';
 
-export default function Chat({id, profilePic, username, timestamp, imageUrl, read }) {
+export default function Chat({ id, profilePic, username, timestamp, imageUrl, read }) {
   return (
     <div className="chat">
-       {/* <Avatar src={profilePic} /> */}
-       <div className="chat__info">
-         <h4>{username}</h4>
-         <p>Tap to view - {new Date(timestamp?.toDate()).toUTCString()}</p>
-       </div>
+      <Avatar src={profilePic} className="chat_avatar" />
+      <div className="chat__info">
+        <h4>{username}</h4>
+        <p>Tap to view - <ReactTimeAgo date={new Date(timestamp?.toDate()).toUTCString()}/></p>
+      </div>
+      {!read && <StopRounded className="chat__readIcon" />}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 
+import TimeAgo from 'javascript-time-ago'
+
+import en from 'javascript-time-ago/locale/en'
+import ru from 'javascript-time-ago/locale/ru'
+
+TimeAgo.addDefaultLocale(en)
+TimeAgo.addLocale(ru)
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
